a95038e7b6ed92c1eacc03fe15ce07b4d21b2539
CAHL92 <dbrito@3dlinkweb.com>
2016-10-18 13:02:22 -0400

    modified access6

--- app.js
+++ app.js
@@ -55,18 +55,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+})
+
 app.use('/', routes);
 app.use('/users', users);
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-};
 
-app.configure(function() {
-    app.use(allowCrossDomain);
-});
 
 app.use(express.static(__dirname + '/public'));
 
