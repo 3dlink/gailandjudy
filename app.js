85da8def8e9964b37c72bed4d37152cd5ce943b1
CAHL92 <dbrito@3dlinkweb.com>
2016-10-18 12:24:05 -0400

    modified access2

--- app.js
+++ app.js
@@ -58,9 +58,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
-app.use(express.bodyParser()).post('/', function(req, res){
-res.header("Access-Control-Allow-Origin", "*").header("Access-Control-Allow-Headers", "X-Requested-With").header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
-res.end("test");
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 app.use(express.static(__dirname + '/public'));
