b296254cafff0704c8058aec82a720093498bc3c
CAHL92 <dbrito@3dlinkweb.com>
2016-10-18 12:42:00 -0400

    modified access4

--- app.js
+++ app.js
@@ -58,25 +58,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', *);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+};
 
-app.all('*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
+app.configure(function() {
+    app.use(allowCrossDomain);
 });
-
-// app.all('*', function(req, res, next) {
-//        res.header("Access-Control-Allow-Origin", "*");
-//        res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//        res.header('Access-Control-Allow-Headers', 'Content-Type');
-//        next();
-// });
 
 app.use(express.static(__dirname + '/public'));
 
