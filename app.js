1aeff2932baf4b2622493fc23d3b4532d985b283
CAHL92 <dbrito@3dlinkweb.com>
2016-10-18 12:28:17 -0400

    modified access3

--- app.js
+++ app.js
@@ -64,6 +64,20 @@ app.use(function(req, res, next) {
   next();
 });
 
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+// app.all('*', function(req, res, next) {
+//        res.header("Access-Control-Allow-Origin", "*");
+//        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//        res.header('Access-Control-Allow-Headers', 'Content-Type');
+//        next();
+// });
+
 app.use(express.static(__dirname + '/public'));
 
 app.use(function(req,res,next){
